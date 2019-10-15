dbd2f2e2c83bd6f789fe670373f93c25f1cc11f5
Tatyana-Kozlikina <kozlikina.t.c@gmail.com>
2019-10-15 13:26:03 +0700

    lesson 4.2 закончен Реализован тест: гость может перейти с главной на страницу логина

--- Проект_модуль_4/test_main_page.py
+++ Проект_модуль_4/test_main_page.py
@@ -1,17 +1,13 @@
 from .pages.main_page import MainPage
-from .pages.login_page import LoginPage
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
     link="http://selenium1py.pythonanywhere.com/"
     page=MainPage(browser,link)
     page.open()
     page.go_to_login_page()
-    page_login=LoginPage(browser,browser.current_url)
-    page_login.should_be_login_page()
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
